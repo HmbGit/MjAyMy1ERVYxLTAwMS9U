fd959014d35e5f806846846d29db9deaca985e92
h.babchia <bobhaytem@gmail.com>
2023-01-20 16:30:00 +0100

    feat(SquareService): add CrossOrigin

--- src/main/java/com/demo/game/controller/SquareController.java
+++ src/main/java/com/demo/game/controller/SquareController.java
@@ -3,19 +3,18 @@ package com.demo.game.controller;
 import com.demo.game.model.Square;
 import com.demo.game.service.SquareService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/square")
 public class SquareController {
 
     @Autowired
     private SquareService squareService;
 
-    @PostMapping(value = "/create")
+
+    @PostMapping(value = "/create" ,produces = "application/json")
     public String createSquare(@RequestBody Square square){
         return  squareService.saveSquare(square);
     }
